b043c5683ee4b6eb97c84d952450e4d641b6e376
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2024-12-27 22:53:37 +0900

    詳細文を修正

--- algorithm/01.sort/comb/index.py
+++ algorithm/01.sort/comb/index.py
@@ -1,36 +1,36 @@
 """
 Comb Sort
-
+Comb sort is a relatively simpl sorting algorithm originally designed by Wlodzimierz Dobosiewixz and Artur Borowy in 1980,
+later rediscovered (and given the name "Combsort") by Stephen Lacey and Richard Box in 1991. Comb sort improves on
+bubble sort in the same way that Shellsort improves on insertion sort,
+in that they both allow elements that start far away from their intended position to move more than one space per swap.
 
 フロー：
-1. 前方向に進むパス
-  - 配列の最初から隣接する要素を比較し、必要に応じて交換する
-  - 最大値がリストの末尾に移動する 
-2. 後方向に進むパス
-  - 配列の最後から隣接する要素を比較し、必要に応じて交換する
-  - 最小値がリストの先頭に移動する
-3. 範囲を狭めながら繰り返す
-  - ソート済み部分を除外して残りの要素に対して同様の処理を繰り返す
-4. 早期終了の判定
-  - 交換が一度も発生しなければソート完了と判定し、終了する
+1. 初期設定
+  - 配列の長さを初期ギャップとして設定する
+2. ギャップの縮小
+  - ギャップを1.3倍に縮小しながら繰り返す
+3. 比較と交換
+  - キャップ分だけ離れた要素を比較し、必要に応じて交換する
+4. 終端判定
 
 時間計算量：
-Average: O(n²) (二重ループ)
-Worst:   O(n²) (逆順の場合)
-Best:    O(n) (すでにソート済みなら1回のパスで終了)
+Average: O(n log n) ギャップの縮小により効率化されている
+Worst:   O(n²)      ギャップが小さくなったとき、バブルソートを同様の動きになる
+Best:    O(n)       すでにソートされている場合は高速終了
 
 空間計算量：
-O(1) (追加のメモリを使用しない)
+O(1) 追加のメモリを使用しないインプレースアルゴリズム
 """
 import random
 from typing import List
 
 def comb_sort(nums: List[int]) -> List[int]:
     len_nums = len(nums)
-    gap = len_nums
-    swapped = True
+    gap = len_nums # 配列サイズで初期化
+    swapped = True 
     while gap != 1:
-        gap = int(gap / 1.3)
+        gap = int(gap / 1.3) # ギャップを1.3倍に縮小　1.3は経験的に最適とされる値のこと
         if gap < 1:
             gap = 1
 
@@ -42,7 +42,6 @@ def comb_sort(nums: List[int]) -> List[int]:
                 swapped = True
         
     return nums
-
 
 if __name__ == "__main__":
     nums = [random.randint(0, 1000) for _ in range(10)]
